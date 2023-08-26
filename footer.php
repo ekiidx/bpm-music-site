dbb007bbec06dc09c799ef85f5aa037fa35803bd
ekiidx <info@vuedesign.co>
2023-08-25 21:41:45 -0400

    added index, header, and footer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<section class="s-social">
+	<div class="row social-content">
+		<div class="column">
+			<ul class="social-list">
+				
+			</ul>
+		</div>
+	</div>
+</section>
+
+<footer class="s-footer">
+
+<div class="row footer-top">
+	<div class="column large-4 medium-5 tab-full">
+		<div class="footer-logo">
+			<a class="site-footer-logo" href="index.php">
+			   <img src="" alt="Homepage">
+			</a>
+		</div>  
+		<p></p>
+	</div>
+	<div class="column large-half tab-full">
+		<div class="row">
+			<div class="column large-7 medium-full">
+				<h4 class="h6">Our Location</h4>
+				<p>Cleveland, Ohio</p>
+
+				<p>
+			  <!--  <a href="https://goo.gl/maps/bc7C7eYtSmnNs6216" target="_blank" class="btn btn--footer">Get Direction</a> -->
+				</p>
+			</div>
+			<div class="column large-5 medium-full">
+				<h4 class="h6">Links</h4>
+				<ul class="footer-list">
+					<li><a href="/">Home</a></li>
+					<li><a href="/about">About</a></li>
+					<li><a href="/events">Events</a></li>
+					<li><a href="/contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div> <!-- end footer-top -->
+
+<div class="row footer-bottom">
+	<div class="column ss-copyright">
+		<span>© Copyright BPM Music <?php echo date('Y'); ?></span> 
+		<span>Design by &nbsp;<a class="vue-design-btn" href="https://vuedesign.co">Vue Design</a></span>
+	</div>
+</div>
+
+</footer>
+
+<script src="<?php echo '/assets/js/jquery-3.2.1.min.js'; ?>"></script>
+<script src="<?php echo '/assets/js/plugins.js'; ?>"></script>
+<script src="<?php echo '/assets/js/main.js'; ?>"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,189 @@
+<?php include 'header.php'; ?>
+
+<main id="page-home">
+
+    <section class="s-hero" data-parallax="scroll" data-image-src="/assets/img/main-hero.jpg" data-natural-width=3000 data-natural-height=2000 data-position-y=center>
+        <div class="row hero-content">
+            <div class="column large-full hero-content__text">
+                <h1>
+                    Your Journey <br>
+                    Through Sound <br>
+                    Starts Here
+                </h1>
+
+                <div class="hero-content__buttons">
+                    <a href="/events" class="btn btn--stroke">Upcoming Events</a>
+                    <a href="/about" class="btn btn--stroke">About Us</a>
+                </div>
+            </div>
+        </div> 
+    </section>
+
+    <section id="about" class="s-about">
+        <div class="row row-y-center about-content">
+            <div class="column large-full medium-full">
+                <h3 class="display-1 subhead"></h3>
+                <p class="lead"></p>
+                <a href="/about" class="btn btn--primary btn--about">More About Us</a>
+            </div>
+
+            <!--<div class="column large-half medium-full">
+                <ul class="about-sched">
+                    <li>
+                        <h4>Main Church Service</h4>
+                        <p>
+                        Sunday - 9:00 AM | 1:00 PM | 4:30 PM <br>
+                        1600 Amphitheatre Parkway, Mt. View, CA, 94043
+                        </p>
+                    </li>
+                    <li>
+                        <h4>Kids Church</h4>
+                        <p>
+                        Sunday - 9:00 AM | 1:00 PM | 4:30 PM <br>
+                        1600 Amphitheatre Parkway, Mt. View, CA, 94043
+                        </p>
+                    </li>
+                    <li>
+                        <h4>Prayer Meeting</h4>
+                        <p>
+                        Saturday - 7:00 PM <br>
+                        1600 Amphitheatre Parkway, Mt. View, CA, 94043
+                        </p>
+                    </li>
+                </ul>
+            </div> -->
+
+        </div>
+    </section>
+
+<!-- <section class="s-connect">
+        <div class="row connect-content">
+            <div class="column large-half tab-full">
+                <h3 class="display-1">Volunteer With Us.</h3>
+                <p>
+                Delectus distinctio fuga commodi quod temporibus consequatur. 
+                Voluptatem dolor vel impedit. Totam ut vel nihil ab. Nostrum ipsa 
+                necessitatibus. Iste voluptatibus qui velit et voluptatem laudantium 
+                et explicabo. Dignissimos ut voluptatum laboriosam nisi fugiat.
+                Nulla dolores voluptate sit unde in doloribus est. Eveniet qui et 
+                quia pariatur consequatur officia facere aut.
+                </p>
+
+                <a href="volunteer.html" class="btn btn--primary h-full-width">I'm Interested</a>
+            </div>
+            <div class="column large-half tab-full">
+                <h3 class="display-1">Join a Connect Group.</h3>
+                <p>
+                Officia earum at quia recusandae. Tempora beatae est 
+                aliquam fugiat sed et. Exercitationem vitae molestiae 
+                officia eos aut id ad. Et exercitationem quae perspiciatis 
+                mollitia. Laborum quasi inventore eaque quia non.
+                Ipsa dignissimos ipsum nisi qui eos et iste magnam. Aut dolorum 
+                mollitia illum. Iste iure similique nobis fuga est amet. 
+                </p>
+
+                <a href="connect-group.html" class="btn btn--primary h-full-width">Learn More</a>
+            </div>
+        </div> 
+    </section> -->
+
+    <section class="s-events">
+        <div class="row events-header">
+            <div class="column">
+                <h2 class="subhead">Upcoming Events</h2>
+            </div>
+        </div> 
+
+                    <div class="row block-large-1-2 block-900-full events-list">
+                        <div class="column events-list__item">
+                            <div>
+                                <h3 class="display-1 events-list__item-title">
+                                    <a href="" title=""></a>
+                                </h3>
+
+                         
+
+                                <ul class="events-list__meta">
+                                    <li class="events-list__meta-date"></li>
+                                    <li class="events-list__meta-time"></li>
+                                    <li class="events-list__meta-location"></li>
+                                </ul>
+
+                                <div class="large-full medium-full"> 
+                                        <a href="" class="btn btn--primary btn--buy-ticket">Buy Tickets</a>
+                                
+                                 
+                                        <a href="" class="btn btn--primary btn--facebook">Facebook Page</a>
+                     
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column events-list__item">
+
+                                <a href="" title="">
+                                    <img src="" alt="" /></a>
+                        </div> 
+                    </div>
+    </section>
+
+    <section class="s-series">
+        <div class="series-img"></div>
+            <div class="row row-y-center series-content">
+                <div class="column large-half medium-full">
+
+                    <h3 class="subhead">Current Mixes</h3>
+                    <h2>Download And Listen To The Latest From The Crew</h2>
+                    <p>Aut sed amet et quis aliquid laborum minus consequatur. Animi repellendus quas. 
+                    Est voluptates minima ut dolorum aliquid sint. Ratione et et molestias rerum 
+                    quibusdam.</p>
+                </div>
+
+            <div class="column large-half medium-full">
+                <div class="series-content__buttons">
+                    <a href="/contact" class="btn btn--large h-full-width">Send Us A Mix</a>
+                    <a href="https://soundcloud.com/momsbassment" class="btn btn--large h-full-width">Listen To Our Soundcloud</a>
+                </div>
+
+                <div class="series-content__subscribe">
+                    <p>Never miss a mix. Subscribe to our YouTube and Podcast channels.</p>
+                    
+                    <ul class="series-content__subscribe-links">
+                        <li class="ss-apple-podcast"><a href="#0">Apple Podcast</a></li>
+                        <li class="ss-spotify"><a href="#0">Spotify</a></li>
+                        <li class="ss-soundcloud"><a href="https://soundcloud.com/momsbassment">SoundCloud</a></li>
+                        <li class="ss-youtube"><a href="https://www.youtube.com/watch?v=0vxu3MCu7uA">Youtube</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="column large-full medium-full soundcloud-container">
+              
+                    <div class="music-container">
+                        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1237021741&color=%232be6ab&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
+
+               
+                    <div class="music-container">
+                        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1237007842&color=%232be6ab&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
+
+            
+                    <div class="music-container">
+                        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1175242789&color=%232be6ab&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
+
+            
+                    <div class="music-container">
+                        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1173809806&color=%232be6ab&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                    </div>
+
+
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<?php include 'footer.php';
