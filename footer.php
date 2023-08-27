130e80dc501a0dcd51f628676fba4060068ee0ba
ekiidx <info@vuedesign.co>
2023-08-27 15:53:12 -0400

    Updates to site

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="colophon" class="site-footer">
-	<div class="container">
+	<div class="container footer-container">
 		<div class="row">
 			<div class="col-4">
 				<img src="">
@@ -70,7 +70,7 @@
 	</div>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php include 'header.php'; ?>
 
-<style>
-    #welcome {
-        background: url('/assets/img/welcome-bg.png') no-repeat;
-    }
-    #contact {
-        background: url('/assets/img/contact-bg.jpg') no-repeat;
-    }
-</style>
-
 <main id="home">
 
 <section id="hero-main">
@@ -20,8 +11,9 @@
     </div>-->
 
     <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
 
+        <!-- Item -->
+        <div class="carousel-item active" data-bs-interval="10000">
             <div class="hero-background-0"></div>
             <div class="background-filter-0"></div>
             <div class="background-filter-0a"></div>
@@ -29,62 +21,64 @@
 
             <div class="container reveal animate__animated animate__fadeIn">
                 <div class="hero-subtitle-container">
-                    <span class="hero-subtitle-1 uppercase blue center teko block">Bottom Title</span>
+                    <span class="hero-subtitle-1 text-uppercase text-center d-block">Bottom Title</span>
                 </div>
 
                 <div class="hero-title-container">
-                    <span class="hero-title uppercase center teko block">Hero Title 1</span> 
+                    <span class="hero-title text-uppercase text-center d-block">Hero Title 1</span> 
                 </div>
 
-                    <a class="red-btn teko uppercase" href="https://www.facebook.com">Hear More</a>
-                    <a class="blue-btn teko black uppercase" href="/contact.php">Contact Us</a>
+                    <a class="text-uppercase" href="https://www.facebook.com">Hear More</a>
+                    <a class="text-uppercase" href="/contact.php">Contact Us</a>
             </div>
 
         </div>
-        <div class="carousel-item" data-bs-interval="10000">
 
+        <!-- Item -->
+        <div class="carousel-item" data-bs-interval="10000">
             <div class="hero-background-1"></div>
             <div class="background-filter-1"></div>
 
             <div class="container reveal animate__animated animate__fadeIn">
                 <div class="hero-subtitle-container">
-                    <span class="hero-subtitle-1 uppercase blue center teko block">Bottom Title</span>
+                    <span class="hero-subtitle-1 text-uppercase text-center d-block">Bottom Title</span>
                 </div>
 
                 <div class="hero-title-container">
-                    <span class="hero-title uppercase center teko block">Hero Title 1</span> 
+                    <span class="hero-title text-uppercase text-center d-block">Hero Title 1</span> 
                 </div>
-                    <a class="red-btn teko uppercase" href="https://www.facebook.com">Hear More</a>
-                    <a class="blue-btn teko black uppercase" href="/contact.php">Contact Us</a>
+                    <a class="text-uppercase" href="https://www.facebook.com">Hear More</a>
+                    <a class="text-uppercase" href="/contact.php">Contact Us</a>
             </div>
         </div>
-        <div class="carousel-item" data-bs-interval="10000">
 
+        <!-- Item -->
+        <div class="carousel-item" data-bs-interval="10000">
             <div class="hero-background-2"></div>
             <div class="background-filter-2"></div>
 
             <div class="container reveal animate__animated animate__fadeIn">
                 <div class="hero-subtitle-container">
-                    <span class="hero-subtitle-1 uppercase blue center teko block">Bottom Title</span>
+                    <span class="hero-subtitle-1 text-uppercase text-center d-block">Bottom Title</span>
                 </div>
 
                 <div class="hero-title-container">
-                    <span class="hero-title uppercase center teko block">Hero Title 1</span> 
+                    <span class="hero-title text-uppercase text-center d-block">Hero Title 1</span> 
                 </div>
-                    <a class="red-btn teko uppercase" href="https://www.facebook.com">Hear More</a>
-                    <a class="blue-btn teko black uppercase" href="/contact">Contact Us</a>
+                    <a class="text-uppercase" href="https://www.facebook.com">Hear More</a>
+                    <a class="text-uppercase" href="/contact">Contact Us</a>
             </div>
         </div>
     </div>
 
-    <!--<button class="carousel-control-prev" type="button" data-bs-target="#main-carousel" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#main-carousel" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#main-carousel" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-    </button>-->
+    </button>
     </div>
 </section>
 
@@ -101,8 +95,9 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <span class="subtitle red uppercase teko">Check Out</span>
-                <h1 class="title white uppercase teko">Our Next Event</h1>
+                <span class="subtitle text-uppercase">Check Out</span>
+                <h1 class="title text-uppercase">Our Next Event</h1>
+                <img class="hr" src="/assets/img/hr.jpg">
             </div>
         </div> <?php
 
@@ -118,10 +113,10 @@
 <section id="music">
     <div class="container">
         <div class="row">
-            <div class="col-12 center">
-                <span class="subtitle black uppercase teko">Diversity Playlist</span>
-                <h2 class="title uppercase teko">Recent Mixes & Tracks</h2>
-                <img class="hr" src="/assets/img/wave.gif">
+            <div class="col-12 text-center">
+                <span class="subtitle text-uppercase">Diversity Playlist</span>
+                <h2 class="title text-uppercase">Recent Mixes & Tracks</h2>
+                <img class="hr" src="/assets/img/hr.jpg">
 
                 <div class="soundcloud-container">
 
@@ -146,10 +141,10 @@
 <section id="gallery">
     <div class="container gallery-container">
          <div class="row">
-            <div class="col-12 center">
-                <span class="subtitle red uppercase center teko">Gallery</span>
-                <h2 class="title uppercase center teko">Latest Photos</h2>
-                <img class="hr" src="/assets/img/wave.gif">
+            <div class="col-12 text-center">
+                <span class="subtitle text-uppercase text-center">Gallery</span>
+                <h2 class="title text-uppercase text-center">Latest Photos</h2>
+                <img class="hr" src="/assets/img/hr.jpg">
             </div>
             <div class="col-6 col-lg-3">
                 <a href="/assets/img/shirt-1.png"><img class="gallery-img" src="/assets/img/shirt-1.png"></a>
@@ -182,14 +177,15 @@
 <section id="contact">
     <div class="container">
         <div class="row">
-            <div class="col-12 center red-border">
-                <span class="subtitle red uppercase teko">Hello</span>
-                <h1 class="title blue uppercase teko">Drop Us A Message</h1>
-                <img class="hr" src="/assets/img/wave.gif">
+            <div class="col-12 text-center">
+                <span class="subtitle text-uppercase">Hello</span>
+                <h1 class="title text-uppercase">Drop Us A Message</h1>
+                <img class="hr" src="/assets/img/hr.jpg">
                 
-                <div class="contact-container">
-                 
-                </div>
+                <form>
+                    <input type="text">
+                    <input type="submit">
+                </form>
                 
             </div>
         </div>
